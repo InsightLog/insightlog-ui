5fc79b93c74d810a8ca94a267141161edfdde472
Sushil <sushil.eshika@gmail.com>
2025-07-17 16:41:15 +0530

    fix: no fallbacks

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {API_BASE_URL} from "../config.js";
 const instance = axios.create({
-  baseURL: API_BASE_URL || "http://localhost:5000/api/v1",
+  baseURL: API_BASE_URL,
 });
 
 instance.interceptors.request.use((config) => {
